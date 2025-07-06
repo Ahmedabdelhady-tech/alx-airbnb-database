8c8b36cc9982a9290e63ef345c2d2c2818a6bfe2
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-06 22:28:34 +0300

    task 4

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,4 +1,4 @@
--- 1) Initial complex query: retrieve bookings with user, property & payment details
+-- 1) Initial complex query: retrieve bookings with user, property & payment details (using WHERE and AND)
 SELECT
   b.id            AS booking_id,
   b.created_at    AS booking_date,
@@ -10,22 +10,30 @@ SELECT
   pay.amount      AS payment_amount,
   pay.status      AS payment_status
 FROM
-  bookings AS b
-  INNER JOIN users      AS u   ON b.user_id     = u.id
-  INNER JOIN properties AS p   ON b.property_id = p.id
-  LEFT  JOIN payments   AS pay ON b.id          = pay.booking_id;
+  bookings AS b,
+  users AS u,
+  properties AS p,
+  payments AS pay
+WHERE
+  b.user_id         = u.id
+  AND b.property_id = p.id
+  AND pay.booking_id = b.id;
 
 -- 2) Analyze performance of the initial query
 EXPLAIN ANALYZE
 SELECT
   b.id, u.name, p.title, pay.amount
 FROM
-  bookings AS b
-  INNER JOIN users      AS u   ON b.user_id     = u.id
-  INNER JOIN properties AS p   ON b.property_id = p.id
-  LEFT  JOIN payments   AS pay ON b.id          = pay.booking_id;
+  bookings AS b,
+  users AS u,
+  properties AS p,
+  payments AS pay
+WHERE
+  b.user_id         = u.id
+  AND b.property_id = p.id
+  AND pay.booking_id = b.id;
 
--- 3) Refactored query: drop unused columns & leverage indexing
+-- 3) Refactored query: drop unused columns & leverage indexing (using JOIN syntax)
 SELECT
   b.id           AS booking_id,
   b.created_at   AS booking_date,
