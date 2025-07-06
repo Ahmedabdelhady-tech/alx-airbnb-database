a8a1e274987d7df95c940258d3d25b631e0bbdb9
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-06 21:47:13 +0300

    task 2

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,6 +11,33 @@ GROUP BY
 ORDER BY
   total_bookings DESC;
 
+-- Query 2: Rank properties based on the total number of bookings received using ROW_NUMBER()
+SELECT
+  p.id AS property_id,
+  p.title AS property_title,
+  COUNT(b.id) AS bookings_count,
+  ROW_NUMBER() OVER (ORDER BY COUNT(b.id) DESC) AS booking_rank
+FROM
+  properties AS p
+  LEFT JOIN bookings AS b ON p.id = b.property_id
+GROUP BY
+  p.id, p.title
+ORDER BY
+  booking_rank;
+sql
+-- Query 1: Total number of bookings made by each user
+SELECT
+  u.id AS user_id,
+  u.name AS user_name,
+  COUNT(b.id) AS total_bookings
+FROM
+  users AS u
+  LEFT JOIN bookings AS b ON u.id = b.user_id
+GROUP BY
+  u.id, u.name
+ORDER BY
+  total_bookings DESC;
+
 -- Query 2: Rank properties based on the total number of bookings received
 SELECT
   p.id AS property_id,
