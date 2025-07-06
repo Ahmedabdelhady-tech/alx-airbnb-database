ce053e0b27eadb896a055508c539d3cf75fc7a1b
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2025-07-06 21:33:47 +0300

    joins update

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,7 +4,8 @@ SELECT
   u.*        -- all user fields
 FROM
   bookings AS b
-  INNER JOIN users AS u ON b.user_id = u.id;
+  INNER JOIN users AS u ON b.user_id = u.id
+ORDER BY b.id;
 
 -- Query 2: LEFT JOIN to retrieve all properties and their associated reviews,
 -- including properties without any reviews
@@ -13,7 +14,8 @@ SELECT
   r.*        -- review fields or NULL if no review exists
 FROM
   properties AS p
-  LEFT JOIN reviews AS r ON p.id = r.property_id;
+  LEFT JOIN reviews AS r ON p.id = r.property_id
+ORDER BY p.id;
 
 -- Query 3: FULL OUTER JOIN to retrieve all users and all bookings,
 -- including users without bookings and bookings without linked users
@@ -22,4 +24,5 @@ SELECT
   b.*        -- booking fields or NULL if no booking exists
 FROM
   users AS u
-  FULL OUTER JOIN bookings AS b ON u.id = b.user_id;
+  FULL OUTER JOIN bookings AS b ON u.id = b.user_id
+ORDER BY u.id;
